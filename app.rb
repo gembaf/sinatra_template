797b3acee4eff62da2d608b71bf6cdfaf6327458
gembaf <gembaf.github@gmail.com>
2016-02-05 14:21:54 +0000

    UserAgentに関するモジュールを追加

--- app.rb
+++ app.rb
@@ -3,9 +3,15 @@ require 'sinatra/asset_pipeline'
 require 'sinatra/reloader' if development?
 
 require 'slim'
+require 'sass'
+require 'coffee-script'
+
+Dir[File.dirname(__FILE__) + '/models/**/*.rb'].each { |f| require f }
 
 set :root, File.dirname(__FILE__)
-set :assets_precompile, %w(application.js application.css *.png *.jpg *.svg *.eot *.ttf *.woff)
+set :assets_precompile, %w(application.js application.css
+                           *.png *.jpg *.svg
+                           *.eot *.ttf *.woff)
 set :assets_css_compressor, :sass
 set :assets_js_compressor, :uglifier
 register Sinatra::AssetPipeline
